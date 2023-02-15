6570ded1ae934a76765cb5290cd8958279eb0d10
todorilievski <todor.ilievski@byteworx.de>
2023-02-15 19:13:29 +0100

    Add routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {CustomerListComponent} from "./body/customer-list/customer-list.component";
 
-
+const routes: Routes = [
+  { path: '', redirectTo: '/customers', pathMatch: 'full' },
+  { path: 'customers', component: CustomerListComponent }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
